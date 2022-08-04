7caf1e320e85d0e729b4eebd4205cf344960581a
jailtonmendes <jailtonmendes.silva@gmail.com>
2022-08-04 00:56:38 -0300

    Tela Home - criação do campo de pesquisa e categorias

--- greengrocer/lib/src/home/home_tab.dart
+++ greengrocer/lib/src/home/home_tab.dart
@@ -1,9 +1,25 @@
 import 'package:badges/badges.dart';
 import 'package:flutter/material.dart';
 import 'package:greengrocer/src/config/custom.colors.dart';
+import 'package:greengrocer/src/home/components/category_tile.dart';
 
-class HomeTab extends StatelessWidget {
-  const HomeTab({Key? key}) : super(key: key);
+class HomeTab extends StatefulWidget {
+  HomeTab({Key? key}) : super(key: key);
+
+  @override
+  State<HomeTab> createState() => _HomeTabState();
+}
+
+class _HomeTabState extends State<HomeTab> {
+  List<String> categories = [
+    'Frutas',
+    'Grãos',
+    'Verduras',
+    'Temperos',
+    'Cereais',
+  ];
+
+  String selectedCategory = 'Frutas';
 
   @override
   Widget build(BuildContext context) {
@@ -35,23 +51,86 @@ class HomeTab extends StatelessWidget {
           ),
         ),
         actions: [
-          Badge(
-            badgeColor: CustomColors.customContrastColor,
-            badgeContent: Text('2'),
-            child: IconButton(
-              onPressed: () {},
-              icon: Icon(Icons.shopping_cart),
-              color: CustomColors.customSwatchColor,
+          Padding(
+            padding: const EdgeInsets.only(top: 15, right: 15),
+            child: GestureDetector(
+              onTap: () {},
+              child: Badge(
+                badgeColor: CustomColors.customContrastColor,
+                badgeContent: Text(
+                  '2',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 12,
+                  ),
+                ),
+                child: Icon(
+                  Icons.shopping_cart,
+                  color: CustomColors.customSwatchColor,
+                ),
+              ),
             ),
           ),
         ],
       ),
 
-      //Campo de pesquisa
+      body: Column(
+        children: [
+          //Campo de pesquisa
+          Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: 20,
+              vertical: 10,
+            ),
+            child: TextFormField(
+              decoration: InputDecoration(
+                  filled: true,
+                  fillColor: Colors.white,
+                  isDense: true, //Diminuir mais a altura do campo
+                  hintText: 'Pesquise aaqui...',
+                  hintStyle: TextStyle(
+                    color: Colors.grey.shade400,
+                    fontSize: 14,
+                  ),
+                  prefixIcon: Icon(
+                    Icons.search,
+                    color: CustomColors.customContrastColor,
+                    size: 21,
+                  ),
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(60),
+                      borderSide: const BorderSide(
+                        width: 0,
+                        style: BorderStyle.none,
+                      ))),
+            ),
+          ),
 
-      //Categorias
+          //Categorias
+          Container(
+            padding: EdgeInsets.only(left: 25),
+            height: 40,
+            child: ListView.separated(
+              scrollDirection: Axis.horizontal,
+              itemBuilder: (_, index) {
+                return CategoryTile(
+                  onPressed: () {
+                    setState(() {
+                      selectedCategory = categories[index];
+                    });
+                  },
+                  category: categories[index],
+                  isSelected: categories[index] == selectedCategory,
+                );
+              },
+              separatorBuilder: (_, index) => const SizedBox(width: 10),
+              itemCount: categories.length,
+            ),
+          ),
 
-      //Grid
+          //Grid
+        ],
+      ),
     );
   }
 }
